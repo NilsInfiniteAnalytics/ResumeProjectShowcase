d2d3c3bc3bb7a90bf3b4a326755c036edc6f2783
Christopher Popovich <c.a.popovich@gmail.com>
2024-04-13 20:20:45 -0400

    Begin js functionality integrations.

---
+++ ResumeProjectShowcase/ClassLibrary/Records/JsFunctions.cs
@@ -0,0 +1,7 @@
+﻿namespace ClassLibrary.Records
+{
+    public record JsFunctions
+    {
+        public const string GetWindowSize = "getWindowSize";
+    }
+}
---
+++ ResumeProjectShowcase/WebApp/wwwroot/scripts.js
@@ -0,0 +1,8 @@
+﻿function getWindowSize() {
+    console.log("Resizing: " + `${window.innerWidth} x ${window.innerHeight}`);
+    window.DotNet.invokeMethodAsync("WebApp",
+        "UpdateWindowSize",
+        `${window.innerWidth} x ${window.innerHeight}`);
+}
+
+window.addEventListener("resize", getWindowSize);
