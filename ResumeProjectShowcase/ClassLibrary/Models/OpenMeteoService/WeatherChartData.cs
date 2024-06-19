1e6a7e052524f56081d899bcbfd9ff751a2d5663
Christopher Popovich <c.a.popovich@gmail.com>
2024-06-18 22:35:00 -0400

    Adjust plotting to handle all data within a single plot.

--- ResumeProjectShowcase/ClassLibrary/Models/OpenMeteoService/WeatherChartData.cs
+++ ResumeProjectShowcase/ClassLibrary/Models/OpenMeteoService/WeatherChartData.cs
@@ -4,5 +4,7 @@
     {
         public string? Time { get; set; }
         public double Value { get; set; }
+        public string Type { get; set; } = string.Empty;
+        public string Unit { get; set; } = string.Empty;
     }
 }
