24c71fb28a5cbafd69cd5b1051ad2f5fc86de8f1
Christopher Popovich <c.a.popovich@gmail.com>
2024-06-20 15:07:37 -0400

    Update geocode and openmeteo service with exception logging.

--- ResumeProjectShowcase/ClassLibrary/Modules/GeocodingService/GeocodingService.cs
+++ ResumeProjectShowcase/ClassLibrary/Modules/GeocodingService/GeocodingService.cs
@@ -31,35 +31,49 @@ public class GeocodingService(HttpClient httpClient, ILogger logger) : IGeocodin
         logger.Information($"Requesting city name from Geocoding API: {url}");
         var stopwatch = new Stopwatch();
         stopwatch.Start();
-        var response = await httpClient.GetAsync(url);
-        stopwatch.Stop();
-        logger.Information($"Request took {stopwatch.ElapsedMilliseconds} ms");
-        logger.Information($"Response status code: {response.StatusCode}");
-        if (!response.IsSuccessStatusCode)
+        try
         {
+            var response = await httpClient.GetAsync(url);
+            stopwatch.Stop();
+            logger.Information($"Request took {stopwatch.ElapsedMilliseconds} ms");
+            logger.Information($"Response status code: {response.StatusCode}");
+            if (!response.IsSuccessStatusCode)
+            {
+                return "Unknown";
+            }
+            var geocodingResponse = await response.Content.ReadFromJsonAsync<GeocodingResponse>();
+            // Switch on the status code to handle different scenarios using the enum defined above.
+            switch (geocodingResponse?.Status)
+            {
+                case nameof(ReverseGeocodingStatusCodes.OK):
+                    var cityName = geocodingResponse?
+                        .Results.FirstOrDefault()?
+                        .AddressComponents.FirstOrDefault(ac => ac.Types.Contains("locality"))?.LongName ?? "Unknown";
+                    return cityName;
+                case nameof(ReverseGeocodingStatusCodes.ZERO_RESULTS):
+                    logger.Error($"{geocodingResponse?.Status}");
+                    return "Unknown";
+                case nameof(ReverseGeocodingStatusCodes.OVER_QUERY_LIMIT):
+                    logger.Error($"{geocodingResponse?.Status}");
+                    return "Unknown";
+                case nameof(ReverseGeocodingStatusCodes.REQUEST_DENIED):
+                    logger.Error($"{geocodingResponse?.Status}");
+                    return "Unknown";
+                case nameof(ReverseGeocodingStatusCodes.INVALID_REQUEST):
+                    logger.Error($"{geocodingResponse?.Status}");
+                    return "Unknown";
+                case nameof(ReverseGeocodingStatusCodes.UNKNOWN_ERROR):
+                    logger.Error($"{geocodingResponse?.Status}");
+                    return await GetCityNameAsync(latLng);
+                default:
+                    return "Unknown";
+            }
+        }
+        catch (HttpRequestException e)
+        {
+            logger.Error(e, "Error while requesting city name from Geocoding API");
             return "Unknown";
         }
-        var geocodingResponse = await response.Content.ReadFromJsonAsync<GeocodingResponse>();
-        // Switch on the status code to handle different scenarios using the enum defined above.
-        switch (geocodingResponse?.Status)
-        {
-            case nameof(ReverseGeocodingStatusCodes.OK):
-                var cityName = geocodingResponse?
-                    .Results.FirstOrDefault()?
-                    .AddressComponents.FirstOrDefault(ac => ac.Types.Contains("locality"))?.LongName ?? "Unknown";
-                return cityName;
-            case nameof(ReverseGeocodingStatusCodes.ZERO_RESULTS):
-                return "Unknown";
-            case nameof(ReverseGeocodingStatusCodes.OVER_QUERY_LIMIT):
-                return "Unknown";
-            case nameof(ReverseGeocodingStatusCodes.REQUEST_DENIED):
-                return "Unknown";
-            case nameof(ReverseGeocodingStatusCodes.INVALID_REQUEST):
-                return "Unknown";
-            case nameof(ReverseGeocodingStatusCodes.UNKNOWN_ERROR):
-                return await GetCityNameAsync(latLng);
-            default:
-                return "Unknown";
-        }
+        
     }
 }
--- ResumeProjectShowcase/ClassLibrary/Modules/OpenMeteoService/OpenMeteoService.cs
+++ ResumeProjectShowcase/ClassLibrary/Modules/OpenMeteoService/OpenMeteoService.cs
@@ -30,54 +30,61 @@ public sealed class OpenMeteoService : IOpenMeteoService
         query["start_date"] = startDateString;
         query["end_date"] = endDateString;
         query["hourly"] = "temperature_2m,relative_humidity_2m,surface_pressure";
-
         var requestUri = $"{_archiveApiUrl}?{query.ToString()}";
         _logger.Information($"Requesting weather data from OpenMeteo API: {requestUri}");
         var stopwatch = new Stopwatch();
         stopwatch.Start();
-        var response = await _httpClient.GetAsync(requestUri);
-        stopwatch.Stop();
-        _logger.Information($"Request took {stopwatch.ElapsedMilliseconds} ms");
-        _logger.Information($"Response status code: {response.StatusCode}");
-
-        if (!response.IsSuccessStatusCode)
-        {
-            return null;
-        }
-
-        var stringData = await response.Content.ReadAsStringAsync();
-        using var document = JsonDocument.Parse(stringData);
-        var root = document.RootElement;
         try
         {
-            var weatherData = new WeatherData
+            var response = await _httpClient.GetAsync(requestUri);
+            stopwatch.Stop();
+            _logger.Information($"Request took {stopwatch.ElapsedMilliseconds} ms");
+            _logger.Information($"Response status code: {response.StatusCode}");
+
+            if (!response.IsSuccessStatusCode)
             {
-                Latitude = root.GetProperty("latitude").GetDouble(),
-                Longitude = root.GetProperty("longitude").GetDouble(),
-                Timezone = root.GetProperty("timezone").GetString()!,
-                TimezoneAbbreviation = root.GetProperty("timezone_abbreviation").GetString()!,
-                UtcOffsetSeconds = root.GetProperty("utc_offset_seconds").GetInt32(),
-                HourlyWeatherDataUnits = new HourlyWeatherDataUnits
+                return null;
+            }
+            var stringData = await response.Content.ReadAsStringAsync();
+            using var document = JsonDocument.Parse(stringData);
+            var root = document.RootElement;
+            try
+            {
+                var weatherData = new WeatherData
                 {
-                    Time = root.GetProperty("hourly_units").GetProperty("time").GetString()!,
-                    Temperature2m = root.GetProperty("hourly_units").GetProperty("temperature_2m").GetString()!,
-                    RelativeHumidity2m = root.GetProperty("hourly_units").GetProperty("relative_humidity_2m").GetString()!,
-                    SurfacePressure = root.GetProperty("hourly_units").GetProperty("surface_pressure").GetString()!
-                },
-                HourlyWeatherDataLists = new HourlyWeatherDataLists
-                {
-                    Time = root.GetProperty("hourly").GetProperty("time").EnumerateArray().Select(x => x.GetString()!).ToList(),
-                    Temperature2m = root.GetProperty("hourly").GetProperty("temperature_2m").EnumerateArray().Select(x => x.GetDouble()).ToList(),
-                    RelativeHumidity2m = root.GetProperty("hourly").GetProperty("relative_humidity_2m").EnumerateArray().Select(x => x.GetDouble()).ToList(),
-                    SurfacePressure = root.GetProperty("hourly").GetProperty("surface_pressure").EnumerateArray().Select(x => x.GetDouble()).ToList()
-                }
-            };
-            return weatherData;
+                    Latitude = root.GetProperty("latitude").GetDouble(),
+                    Longitude = root.GetProperty("longitude").GetDouble(),
+                    Timezone = root.GetProperty("timezone").GetString()!,
+                    TimezoneAbbreviation = root.GetProperty("timezone_abbreviation").GetString()!,
+                    UtcOffsetSeconds = root.GetProperty("utc_offset_seconds").GetInt32(),
+                    HourlyWeatherDataUnits = new HourlyWeatherDataUnits
+                    {
+                        Time = root.GetProperty("hourly_units").GetProperty("time").GetString()!,
+                        Temperature2m = root.GetProperty("hourly_units").GetProperty("temperature_2m").GetString()!,
+                        RelativeHumidity2m = root.GetProperty("hourly_units").GetProperty("relative_humidity_2m").GetString()!,
+                        SurfacePressure = root.GetProperty("hourly_units").GetProperty("surface_pressure").GetString()!
+                    },
+                    HourlyWeatherDataLists = new HourlyWeatherDataLists
+                    {
+                        Time = root.GetProperty("hourly").GetProperty("time").EnumerateArray().Select(x => x.GetString()!).ToList(),
+                        Temperature2m = root.GetProperty("hourly").GetProperty("temperature_2m").EnumerateArray().Select(x => x.GetDouble()).ToList(),
+                        RelativeHumidity2m = root.GetProperty("hourly").GetProperty("relative_humidity_2m").EnumerateArray().Select(x => x.GetDouble()).ToList(),
+                        SurfacePressure = root.GetProperty("hourly").GetProperty("surface_pressure").EnumerateArray().Select(x => x.GetDouble()).ToList()
+                    }
+                };
+                return weatherData;
+            }
+            catch (Exception e)
+            {
+                _logger.Error(e, "Error while parsing weather data from OpenMeteo API");
+                return null;
+            }
         }
-        catch (Exception e)
+        catch (HttpRequestException e)
         {
-            System.Diagnostics.Trace.TraceError($"Error: {e.Message}");
+            _logger.Error(e, "Error while requesting weather data from OpenMeteo API");
             return null;
         }
+
     }
 }
